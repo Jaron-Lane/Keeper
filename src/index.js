32387a03101cd66573db47fb5f5b56a9ad76e066
Jaron-Lane <jaronwohlwend@icloud.com>
2020-12-09 15:43:46 -0600

    set up Auth, Login, and Register form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Keeper } from './Keeper';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Keeper />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
